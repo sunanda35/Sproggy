02dd880c0ec0ff6ce51367a9f5b2b564fa863b86
sunanda35 <sunandasamanta60@gmail.com>
2022-05-15 11:17:33 +0530

    restrict landscape mood and add useful packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sproggy/routes/route_generator.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
